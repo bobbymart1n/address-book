11f04ddaa4602c28c5418385e2145b2d0b482c63
Bobby Martin and Luke Stockburger <bobby@>
2018-04-02 10:55:10 -0700

    add address keys to contact constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 //business
-function Contact(first, last) {
+function Contact(first, last, street, city, state, zip) {
   this.firstName = first;
   this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
 }
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-
 };
+
+
 
 // user interface logic
 $(document).ready(function() {
@@ -14,18 +19,31 @@ $(document).ready(function() {
     event.preventDefault();
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName)
+    var inputtedStreet = $('input#street').val();
+    var inputtedCity = $('input#city').val();
+    var inputtedState = $('input#state').val();
+    var inputtedZip = $('input#zip').val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState, inputtedZip);
 
     $("#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#street").val("");
+    $("input#city").val("");
+    $("input#state").val("");
+    $("input#zip").val("");
 
     $(".contact").last().click(function () {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".street").text(newContact.street);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
+      $(".zip").text(newContact.zip);
     });
   });
 });
